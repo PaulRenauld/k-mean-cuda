f04531e69affaf3ac26d975faf1e68b47aaec22d
MathieuChevalley <m.chevalley97@gmail.com>
2019-05-04 19:20:00 -0400

    ben

--- code/benchmark.py
+++ code/benchmark.py
@@ -44,7 +44,8 @@ print(len(time_gpu[500]))
 ax.plot(x1, y1)
 ax.plot(x2, y2)
 ax.set(xlabel='Number of data points', ylabel='Execution time')
-plt.show()
+#plt.show()
+plt.savefig('scale_n.png')
 
 # Scale k
 fig, ax = plt.subplots()
@@ -52,7 +53,8 @@ new = [(k, v[500000]) for k, v in time_gpu.items()]
 x, y = zip(*sorted(new))
 ax.set(xlabel='Number of clusters', ylabel='Execution time')
 ax.plot(x, y)
-plt.show()
+#plt.show()
+plt.savefig('scale_k.png')
 
 # heat map
 m = np.zeros((6, 6))
@@ -65,11 +67,14 @@ plt.imshow(m[1:,1:], cmap='hot', interpolation='nearest')
 plt.colorbar()
 plt.xticks(range(0, 5), range(100, 510, 100))
 plt.yticks(range(0, 5), range(100000, 1000001, 200000))
-plt.show()
+#plt.show()
+plt.savefig('map.png')
 
 # K discovery
 subprocess.call(['python3', 'generate_dataset.py', '-n', '500000', '-k', '50', '-o', file_input, '-r', '10', '-v', '20'])
-time_c = int(subprocess.check_output([seq, '-o', 'res', '-i', file_input, '-a', '-m', '5', '-M', '100', '-s', '2'])[0])
-time_p = int(subprocess.check_output([par, '-o', 'res', '-i', file_input, '-a', '-m', '5', '-M', '100', '-s', '2'])[0])
+output = subprocess.check_output([seq, '-o', 'res', '-i', file_input, '-a', '-m', '5', '-M', '100', '-s', '2']).split('\n')
+time_c = int(output[-2])
+output = subprocess.check_output([par, '-o', 'res', '-i', file_input, '-a', '-m', '5', '-M', '100', '-s', '2']).split('\n')
+time_p = int(output[-2])
 
 print('speedup : ' + str(time_p / (1.0 * time_c)))
